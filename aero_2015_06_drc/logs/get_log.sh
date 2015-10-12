a0cad0d3678d0c241a921804bfe1c697301d9b7f
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2015-10-12 15:38:22 +0900

    deleted files not maintained for months including DRC specific files

--- aero_2015_06_drc/logs/get_log.sh
+++
@@ -1,5 +0,0 @@
-# grep last roseus
-line=$(grep -n 'roseus' $1 | tail -1 | cut -f1 -d:)
-tail -n +$line $1 | grep "irteusgl" | cut -f2- -d'$' | sed "{s/ /${2}/}" | sed "s/$/${3}/" > remade_log.txt
-sed -i '/send \*ri\* :angle-vector/d' remade_log.txt
-sed -i '/()/d' remade_log.txt
