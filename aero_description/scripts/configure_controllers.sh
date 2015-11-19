ee22da0299cf2bf95c8d258b8beaedf741c230de
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2015-11-19 19:25:24 +0900

    controller cfg now takes dependent file construction when directory has Main.cc

--- aero_description/scripts/configure_controllers.sh
+++ aero_description/scripts/configure_controllers.sh
@@ -33,54 +33,67 @@ do
     source=$(echo $reference | cut -d '/' -f2)
     copy_from_file="$(rospack find aero_description)/${robot}/controllers/${source}"
     copy_to_dir="$(rospack find aero_description)/../aero_startup/${executable_dir}"
-    if [[ $proto == "&" ]] # depends on other configuration files
+    if [[ $proto == "&" ]] # requires test (optional)
     then
-	${copy_to_dir}/depends $executable_name $copy_from_file
-    elif [[ $proto == "+" ]] # does not depend on other configurations
-    then
-	# copy controller file
-	output_file="${copy_to_dir}/${source}"
-	cp $copy_from_file $output_file
-	sed -i "1i\/*" $output_file
-	sed -i "2i\ * This file auto-generated from script. Do not Edit!" $output_file
-	sed -i "3i\ * Original : aero_description/${robot}/controllers/${source}" $output_file
-	sed -i "4i\*/" $output_file
+	${copy_to_dir}/.test $copy_from_file
+    fi
+    # copy controller file
+    output_file="${copy_to_dir}/${source}"
+    cp $copy_from_file $output_file
+    sed -i "1i\/*" $output_file
+    sed -i "2i\ * This file auto-generated from script. Do not Edit!" $output_file
+    sed -i "3i\ * Original : aero_description/${robot}/controllers/${source}" $output_file
+    sed -i "4i\*/" $output_file
 
-	# add executable to CMakeLists.txt
-	write_to_line=$delete_from_line
-	echo "add_executable(aero_${executable_name}_controller_node" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+    # add executable to CMakeLists.txt
+    write_to_line=$delete_from_line
+    echo "add_executable(aero_${executable_name}_controller_node" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+    write_to_line=$(($write_to_line + 1))
+    includes_main=$(find $copy_to_dir -name Main.cc 2>/dev/null)
+    if [[ $includes_main != "" ]]
+    then
+	cc_files=$(find $copy_to_dir -name "*.cc" | xargs -0 -I{} echo "{}" | awk -F/ '{print $NF}')
+	num_of_cc_files=$(find $copy_to_dir -name "*.cc" | wc -l)
+	for (( num=1; num<=${num_of_cc_files}; num++ ))
+	do
+	    file=$(echo $cc_files | awk '{print $'$num'}')
+	    echo "${tab2}${executable_dir}/${file}" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+	    write_to_line=$(($write_to_line + 1))
+	done
+	echo "${tab2})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
 	write_to_line=$(($write_to_line + 1))
+    else
 	echo "${tab2}${executable_dir}/${source})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
 	write_to_line=$(($write_to_line + 1))
-	echo "target_link_libraries(aero_${executable_name}_controller_node \${catkin_LIBRARIES} \${Boost_LIBRARIES})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+    fi
+    echo "target_link_libraries(aero_${executable_name}_controller_node \${catkin_LIBRARIES} \${Boost_LIBRARIES})\n" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
 
-	# add srv files if required
-	check_for_srv=$(grep "@define srv" $copy_from_file)
-	if [[ "$check_for_srv" != "" ]]
+    # add srv files if required
+    check_for_srv=$(grep "@define srv" $copy_from_file)
+    if [[ "$check_for_srv" != "" ]]
+    then
+	# create srv file
+	mkdir $(rospack find aero_startup)/srv
+	srv_name=$(grep "<aero_startup/" $copy_from_file | cut -d '/' -f2 | cut -d '.' -f1)
+	srv_file="$(rospack find aero_startup)/srv/${srv_name}.srv"
+	awk "/@define srv/,/\*\//" $copy_from_file > $srv_file
+	delete_to_line=$(grep -n -m 1 "\*/" $srv_file | cut -d ':' -f1)
+	sed -i "/@define/d" $srv_file
+	sed -i "/\*\//d" $srv_file
+	sed -i 's/^[ \t]*//' $srv_file
+	# delete srv definitions from output_file
+	delete_from_line=$(grep -n -m 1 "@define srv" $output_file | cut -d ':' -f1)
+	delete_from_line=$(($delete_from_line - 1))
+	delete_to_line=$(($delete_from_line + $delete_to_line))
+	sed -i "${delete_from_line},${delete_to_line}d" $output_file
+	# add srv generation to CMakeLists.txt
+	check_if_exists=$(grep "${srv_name}.srv" $cmake_file)
+	if [[ "$check_if_exists" == "" ]]
 	then
-	    # create srv file
-	    mkdir $(rospack find aero_startup)/srv
-	    srv_name=$(grep "<aero_startup/" $copy_from_file | cut -d '/' -f2 | cut -d '.' -f1)
-	    srv_file="$(rospack find aero_startup)/srv/${srv_name}.srv"
-	    awk "/@define srv/,/\*\//" $copy_from_file > $srv_file
-	    delete_to_line=$(grep -n -m 1 "\*/" $srv_file | cut -d ':' -f1)
-	    sed -i "/@define/d" $srv_file
-	    sed -i "/\*\//d" $srv_file
-	    sed -i 's/^[ \t]*//' $srv_file
-	    # delete srv definitions from output_file
-	    delete_from_line=$(grep -n -m 1 "@define srv" $output_file | cut -d ':' -f1)
-	    delete_from_line=$(($delete_from_line - 1))
-	    delete_to_line=$(($delete_from_line + $delete_to_line))
-	    sed -i "${delete_from_line},${delete_to_line}d" $output_file
-	    # add srv generation to CMakeLists.txt
-	    check_if_exists=$(grep "${srv_name}.srv" $cmake_file)
-	    if [[ "$check_if_exists" == "" ]]
-	    then
-		    sed -i "s/set(GENERATE_SRV)/set(GENERATE_SRV 1)/g" $cmake_file
-		    write_to_line=$(grep -n -m 1 "auto-add services" $cmake_file | cut -d ':' -f1)
-		    write_to_line=$(($write_to_line + 3))
-		    echo "${tab2}${tab2}${srv_name}.srv" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
-	    fi
+	    sed -i "s/set(GENERATE_SRV)/set(GENERATE_SRV 1)/g" $cmake_file
+	    write_to_line=$(grep -n -m 1 "auto-add services" $cmake_file | cut -d ':' -f1)
+	    write_to_line=$(($write_to_line + 3))
+	    echo "${tab2}${tab2}${srv_name}.srv" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
 	fi
     fi
 done < $input_file
