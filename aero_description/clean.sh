c37af41c412b742d177c8acc03a7595589585ace
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2015-11-19 19:39:48 +0900

    added clean.sh

---
+++ aero_description/clean.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cmake_file="$(rospack find aero_description)/../aero_startup/CMakeLists.txt"
+
+# delete controllers from CMakeLists.txt
+
+delete_from_line=$(grep -n -m 1 ">>> add controllers" $cmake_file | cut -d ':' -f1)
+delete_from_line=$(($delete_from_line + 1))
+delete_to_line=$(grep -n -m 1 "<<< add controllers" $cmake_file | cut -d ':' -f1)
+
+if [[ $delete_to_line -ne $delete_from_line ]]
+then
+    delete_to_line=$(($delete_to_line - 1))
+    sed -i "${delete_from_line},${delete_to_line}d" $cmake_file
+fi
+
+# delete srv
+
+num_of_srvs=$(grep ".srv" $cmake_file | wc -l)
+delete_line=$(grep -n -m 1 "auto-add services" $cmake_file | cut -d ':' -f1)
+delete_line=$(($delete_line + 3))
+for (( num=1; num<=${num_of_srvs}; num++ ))
+do
+    sed -i "${delete_line}d" $cmake_file
+done
+sed -i "s/set(GENERATE_SRV 1)/set(GENERATE_SRV)/g" $cmake_file
+
+# delete all auto-generated files
+
+cd "$(rospack find aero_description)/../aero_startup"
+
+delete_files=$(grep -r " \* This file auto-generated" | grep -v 'sed' | cut -d: -f1)
+num_of_files=$(grep -r " \* This file auto-generated" | grep -v 'sed' | wc -l)
+for (( num=1; num<=${num_of_files}; num++ ))
+do
+    file=$(echo $delete_files | awk '{print $'$num'}' | xargs rm)
+done
