35532b8d81673b0e6e5ed4efd38374b25850519b
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2015-11-27 19:52:36 +0900

    added files for iREX demo

--- aero_startup/sandbox/IREX/ColorMemorizer.cc
+++ aero_startup/sandbox/IREX/ColorMemorizer.cc
@@ -1,32 +1,49 @@
 #include <ros/ros.h>
 #include <math.h>
+#include <chrono>
 #include "sensor_msgs/PointCloud2.h"
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/conversions.h>
 #include <pcl_ros/transforms.h>
-#include <pr2_controllers_msgs/JointTrajectoryControllerState.h>
-#include <trajectory_msgs/JointTrajectory.h>
-#include <dynamic_reconfigure/IntParameter.h>
-#include <dynamic_reconfigure/Reconfigure.h>
-#include <dynamic_reconfigure/Config.h>
-#include <std_msgs/Int32.h>
+#include <aero_startup/PointXYZ.h>
+#include <aero_startup/PointHSIArray.h>
+
+/*
+  @define srv 1
+  float32 x_cap
+  float32 y_cap
+  float32 z_cap
+  float32 x
+  float32 y
+  float32 z
+  ---
+  int8 status
+*/
+
+/*
+  @define srv 2
+  int8 size
+  int8[] h_cap_v
+  uint8[] s_cap_v
+  uint8[] i_cap_v
+  int8[] h_v
+  uint8[] s_v
+  uint8[] i_v
+  float32 time
+  ---
+  int8 status
+*/
 
 static const int NUM_OF_PALETTES = 8; // number of dominant colors to find
 static const int NUM_OF_LAYERS = 3; // log2 NUM_OF_PALETTES
 // number of colors in palette that should match the object color
-static const int VALID_MATCH = 4;
+static const int VALID_MATCH = 1;
 // only look for objects with more than this many points
 static const int MINIMUM_POINTS = 100;
 
-ros::Publisher robot_pose_pub;
-ros::Subscriber joint_subscriber;
 ros::Subscriber cloud_subscriber;
-ros::Subscriber person_subscriber;
-
-bool in_wait;
-bool in_check;
 
 struct rgb
 {
@@ -37,21 +54,29 @@ struct rgb
 
 struct hsi
 {
-  std::string label;
-  int h_max;
-  int h_min;
-  int s_max;
-  int s_min;
-  int i_max;
-  int i_min;
+  int h;
+  int s;
+  int i;
 };
 
-std::vector<hsi> samples = {
-  {"RED",  17, 0,  255, 235,  255, 88},
-  {"GREEN",  97, 46,  194, 42,  183, 106},
-  {"BLUE",  -107, -128,  255, 235,  255, 120}
+struct xyz
+{
+  float x;
+  float y;
+  float z;
 };
 
+typedef std::chrono::high_resolution_clock Clock;
+typedef std::chrono::milliseconds milliseconds;
+
+std::vector<hsi> samples_hsi_max;
+std::vector<hsi> samples_hsi_min;
+
+xyz space_min;
+xyz space_max;
+
+bool idle;
+int result;
 
 //////////////////////////////////////////////////
 void MedianCut(std::vector<rgb> _points, int _bucket_idx, int _layer,
@@ -130,9 +155,56 @@ void MedianCut(std::vector<rgb> _points, int _bucket_idx, int _layer,
 };
 
 //////////////////////////////////////////////////
+bool Request(aero_startup::PointHSIArray::Request  &req,
+	     aero_startup::PointHSIArray::Response &res)
+{
+  samples_hsi_max.clear();
+  samples_hsi_min.clear();
+
+  samples_hsi_max.resize(req.size);
+  samples_hsi_min.resize(req.size);
+
+  for (unsigned int i = 0; i < req.size; ++i)
+  {
+    samples_hsi_max[i] = {req.h_cap_v[i], req.s_cap_v[i], req.i_cap_v[i]};
+    samples_hsi_min[i] = {req.h_v[i], req.s_v[i], req.i_v[i]};
+  }
+
+  idle = false;
+  result = -1;
+  Clock::time_point start = Clock::now();
+  while (result < 0)
+  {
+    ros::spinOnce();
+    Clock::time_point now = Clock::now();
+    if (std::chrono::duration_cast<milliseconds>(now - start).count()
+	> (req.time * 1000))
+      break;
+  }
+  idle = true;
+
+  res.status = result;
+  return true;
+};
+
+//////////////////////////////////////////////////
+bool Reconfigure(aero_startup::PointXYZ::Request  &req,
+		 aero_startup::PointXYZ::Response &res)
+{
+  space_min.x = req.x;
+  space_min.y = req.y;
+  space_min.z = req.z;
+  space_max.x = req.x_cap;
+  space_max.y = req.y_cap;
+  space_max.z = req.z_cap;
+  res.status = 1;
+  return true;
+};
+
+//////////////////////////////////////////////////
 void SubscribePoints(const sensor_msgs::PointCloud2::ConstPtr& _msg)
 {
-  if (!in_wait) return;
+  if (idle) return;
 
   pcl::PCLPointCloud2 pcl;
   pcl_conversions::toPCL(*_msg, pcl);
@@ -140,20 +212,17 @@ void SubscribePoints(const sensor_msgs::PointCloud2::ConstPtr& _msg)
       new pcl::PointCloud<pcl::PointXYZRGB>);
   pcl::fromPCLPointCloud2(pcl, *raw);
 
-  Eigen::Vector3f space_min(-0.5, -0.5, -0.5);
-  Eigen::Vector3f space_max(0.5, 0.5, 0.5);
-
   // Find points in search space
 
   std::vector<rgb> extracted;
   extracted.reserve(raw->points.size());
   for (unsigned int i = 0; i < raw->points.size(); ++i)
-    if ((raw->points[i].x > space_min[0]) &&
-        (raw->points[i].x < space_max[0]) &&
-        (raw->points[i].y > space_min[1]) &&
-        (raw->points[i].y < space_max[1]) &&
-        (raw->points[i].z > space_min[2]) &&
-        (raw->points[i].z < space_max[2]))
+    if ((raw->points[i].x > space_min.x) &&
+        (raw->points[i].x < space_max.x) &&
+        (raw->points[i].y > space_min.y) &&
+        (raw->points[i].y < space_max.y) &&
+        (raw->points[i].z > space_min.z) &&
+        (raw->points[i].z < space_max.z))
     {
       rgb tmp = {raw->points[i].r, raw->points[i].g, raw->points[i].b};
       extracted.push_back(tmp);
@@ -163,6 +232,7 @@ void SubscribePoints(const sensor_msgs::PointCloud2::ConstPtr& _msg)
   if (extracted.size() < MINIMUM_POINTS)
   {
     ROS_WARN("not seeing any object! %d points detected", extracted.size());
+    result = -1;
     return; // not enough points
   }
 
@@ -173,63 +243,60 @@ void SubscribePoints(const sensor_msgs::PointCloud2::ConstPtr& _msg)
 
   // Check if detected object matches a known object
 
-  std::vector<int> scores(samples.size(), 0);
+  std::vector<int> scores(samples_hsi_max.size(), 0);
   for (unsigned int i = 0; i < NUM_OF_PALETTES; ++i)
   {
-    ROS_INFO("RGB: %d, %d, %d",
-	     palettes[i].r, palettes[i].g, palettes[i].b);
+    ROS_INFO("RGB: %d, %d, %d", palettes[i].r, palettes[i].g, palettes[i].b);
 
     rgb color = {palettes[i].r, palettes[i].g, palettes[i].b};
     float I_max = std::max({color.r, color.g, color.b});
     float i_min = std::min({color.r, color.g, color.b});
     hsi hsi_color;
     // I
-    hsi_color.i_max = I_max;
+    hsi_color.i = I_max;
     // S
-    if (I_max > 0) hsi_color.s_max = 255 * (1 - i_min / I_max);
-    else hsi_color.s_max = 0;
+    if (I_max > 0) hsi_color.s = 255 * (1 - i_min / I_max);
+    else hsi_color.s = 0;
     // H
     if (static_cast<int>(I_max) == static_cast<int>(i_min))
     {
-      hsi_color.h_max = 0;
+      hsi_color.h = 0;
     }
     else
     {
       if (color.g > color.b)
       {
 	if (color.r > color.g)
-	  hsi_color.h_max = 60 * (color.g - color.b) / (I_max - i_min);
+	  hsi_color.h = 60 * (color.g - color.b) / (I_max - i_min);
 	else
-	  hsi_color.h_max = 60 * (2 + (color.b - color.r) / (I_max - i_min));
+	  hsi_color.h = 60 * (2 + (color.b - color.r) / (I_max - i_min));
       }
       else
       {
 	if (color.r > color.b)
-	  hsi_color.h_max = 360 - 60 * (color.b - color.g) / (I_max - i_min);
+	  hsi_color.h = 360 - 60 * (color.b - color.g) / (I_max - i_min);
 	else
-	  hsi_color.h_max = 60 * (4 + (color.r - color.g) / (I_max - i_min));
+	  hsi_color.h = 60 * (4 + (color.r - color.g) / (I_max - i_min));
       }
-      if (hsi_color.h_max <= 180) // 0 ~ 180 -> 0 ~ 127
-	hsi_color.h_max =
-	  static_cast<int>(hsi_color.h_max / 180.0 * 127);
+      if (hsi_color.h <= 180) // 0 ~ 180 -> 0 ~ 127
+	hsi_color.h =
+	  static_cast<int>(hsi_color.h / 180.0 * 127);
       else
-	hsi_color.h_max = // 180 ~ 360 -> -128 ~ 0
-	  static_cast<int>((hsi_color.h_max - 360) / 180.0 * 128);
+	hsi_color.h = // 180 ~ 360 -> -128 ~ 0
+	  static_cast<int>((hsi_color.h - 360) / 180.0 * 128);
     }
 
-    ROS_INFO("HSI: %d, %d, %d",
-	     hsi_color.h_max, hsi_color.s_max, hsi_color.i_max);
+    ROS_INFO("HSI: %d, %d, %d", hsi_color.h, hsi_color.s, hsi_color.i);
 
-    for (unsigned int j = 0; j < samples.size(); ++j)
-    {
-      if (hsi_color.h_max >= samples[j].h_min &&
-	  hsi_color.h_max <= samples[j].h_max &&
-	  hsi_color.s_max >= samples[j].s_min &&
-	  hsi_color.s_max <= samples[j].s_max &&
-	  hsi_color.i_max >= samples[j].i_min &&
-	  hsi_color.i_max <= samples[j].i_max)
+    for (unsigned int j = 0; j < samples_hsi_max.size(); ++j)
+      if (hsi_color.h >= samples_hsi_min[j].h &&
+          hsi_color.h <= samples_hsi_max[j].h &&
+          hsi_color.s >= samples_hsi_min[j].s &&
+          hsi_color.s <= samples_hsi_max[j].s &&
+          hsi_color.i >= samples_hsi_min[j].i &&
+          hsi_color.i <= samples_hsi_max[j].i)
 	++scores[j];
-    }
+
   }
 
   int most_likely_match;
@@ -244,79 +311,11 @@ void SubscribePoints(const sensor_msgs::PointCloud2::ConstPtr& _msg)
   if (matched_nums < VALID_MATCH)
   {
     ROS_WARN("cannot recognize! %d matches not enough", matched_nums);
+    result = -1;
     return; // not a valid match
   }
 
-  hsi target = samples[most_likely_match];
-  ROS_INFO("recognized %s", target.label.c_str());
-
-  trajectory_msgs::JointTrajectory msg;
-  msg.joint_names = {"neck_y_joint", "neck_p_joint"};
-  msg.points.resize(1);
-  msg.points[0].positions = {0.0, 0.698};
-  robot_pose_pub.publish(msg);
-
-  usleep(static_cast<int32_t>(2 * 1000 * 1000));
-
-  dynamic_reconfigure::ReconfigureRequest req;
-  dynamic_reconfigure::ReconfigureResponse res;
-  dynamic_reconfigure::Config conf;
-  conf.ints.reserve(6);
-  std::vector<std::string> param_names =
-    {"h_limit_max", "h_limit_min", "s_limit_max", "s_limit_min",
-     "i_limit_max", "i_limit_min"};
-  std::vector<int> param_values =
-    {target.h_max, target.h_min, target.s_max, target.s_min,
-     target.i_max, target.i_min};
-  for (unsigned int i = 0; i < 6; ++i)
-  {
-    dynamic_reconfigure::IntParameter tmp;
-    tmp.name = param_names[i];
-    tmp.value = param_values[i];
-    conf.ints.push_back(tmp);
-  }
-  req.config = conf;
-  ros::service::call("/stereo/hsi_color_filter/hsi_filter/set_parameters", req, res);
-
-  in_wait = false;
-  in_check = true;
-};
-
-//////////////////////////////////////////////////
-void SubscribeRobotPose
-(const pr2_controllers_msgs::JointTrajectoryControllerState _msg)
-{
-  // if (!in_check) return;
-
-  // if (fabs(_msg.actual.positions[14]) > 0.78 && // neck_y
-  //     fabs(_msg.actual.positions[15]) < 0.01) // neck_p
-  // {
-  //   in_wait = true;
-  //   in_check = false;
-  // }
-};
-
-//////////////////////////////////////////////////
-void SubscribePersonExists(const std_msgs::Int32 _msg)
-{
-  trajectory_msgs::JointTrajectory msg;
-  msg.joint_names = {"neck_y_joint", "neck_p_joint"};
-  msg.points.resize(1);
-
-  if (_msg.data == 1 && !in_wait && !in_check)
-  {
-    msg.points[0].positions = {-0.79, 0.0};
-    robot_pose_pub.publish(msg);
-    usleep(static_cast<int32_t>(2 * 1000 * 1000));
-    in_wait = true;
-  }
-  else if (_msg.data == 0 && in_wait)
-  {
-    msg.points[0].positions = {0.0, 0.0};
-    robot_pose_pub.publish(msg);
-    usleep(static_cast<int32_t>(2 * 1000 * 1000));    
-    in_wait = false;
-  }
+  result = most_likely_match;
 };
 
 //////////////////////////////////////////////////
@@ -325,16 +324,18 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "color_memorizer");
   ros::NodeHandle nh;
 
-  in_wait = false;
-  in_check = false;
-  joint_subscriber = nh.subscribe("/aero_controller/state",
-				  1, SubscribeRobotPose);
+  space_min = {-0.5, -0.5, -0.5};
+  space_max = {0.5, 0.5, 0.5};
+  idle = true;
+  result = -1;
+
   cloud_subscriber = nh.subscribe("/stereo/points2",
 				  1000, SubscribePoints);
-  person_subscriber = nh.subscribe("/kinect/person_exists",
-				   1, SubscribePersonExists);
-  robot_pose_pub = nh.advertise<trajectory_msgs::JointTrajectory>(
-      "/aero_controller/command", 100);
+
+  ros::ServiceServer service_pa =
+    nh.advertiseService("/color_memorizer/perception_area", Reconfigure);
+  ros::ServiceServer service_cm =
+    nh.advertiseService("/color_memorizer/find_match", Request);
 
   ros::spin();
 
---
+++ aero_startup/sandbox/IREX/GoRun.cc
@@ -0,0 +1,268 @@
+#include <ros/ros.h>
+#include <chrono>
+#include <actionlib/client/simple_action_client.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <move_base_msgs/MoveBaseFeedback.h>
+#include <move_base_msgs/MoveBaseResult.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <tf/transform_listener.h>
+#include <aero_startup/ObjectXYZHSI.h>
+#include <aero_startup/Time.h>
+#include <aero_startup/PointXYZHSI.h>
+
+/*
+  @define srv 1
+  string object
+  float32 x_cap
+  float32 y_cap
+  float32 z_cap
+  float32 x
+  float32 y
+  float32 z
+  int8 h_cap
+  uint8 s_cap
+  uint8 i_cap
+  int8 h
+  uint8 s
+  uint8 i 
+  ---
+  int8 status
+  float32 time
+*/
+
+/*
+  @define srv 2
+  float32 time
+  ---
+  int8 status
+*/
+
+typedef std::chrono::high_resolution_clock Clock;
+typedef std::chrono::milliseconds milliseconds;
+
+static const int RUN_GAIN = 100000; // can be any large number
+static const float GO_AS_NEAR_TO = 0.7; // how close to get to object
+static const int GIVE_UP_TIME = 30000; // ms
+
+actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> *ac;
+ros::ServiceClient client;
+
+//////////////////////////////////////////////////
+void GoPos(float _x, float _y, float _theta)
+{
+  geometry_msgs::Point p;
+  p.x = _x;
+  p.y = _y;
+  p.z = 0;
+
+  geometry_msgs::Quaternion q;
+  q.x = cos(_theta*M_PI/180);
+  q.y = 0;
+  q.z = 0;
+  q.w = sin(_theta*M_PI/180);
+
+  geometry_msgs::Pose pose;
+  pose.position = p;
+  pose.orientation = q;
+
+  geometry_msgs::PoseStamped pstamp;
+  pstamp.pose = pose;
+
+  move_base_msgs::MoveBaseGoal goal;
+  goal.target_pose = pstamp;
+
+  ac->sendGoal(goal);
+};
+
+//////////////////////////////////////////////////
+bool GoForTarget(aero_startup::ObjectXYZHSI::Request  &req,
+		 aero_startup::ObjectXYZHSI::Response &res)
+{
+  // set precise to false
+  aero_startup::PointXYZHSI srv;
+  bool prior_setting;
+  srv.request.x_cap = req.x_cap;
+  srv.request.y_cap = req.y_cap;
+  srv.request.z_cap = req.z_cap;
+  srv.request.x = req.x;
+  srv.request.y = req.y;
+  srv.request.z = req.z;
+  srv.request.h_cap = req.h_cap;
+  srv.request.s_cap = req.s_cap;
+  srv.request.i_cap = req.i_cap;
+  srv.request.h = req.h;
+  srv.request.s = req.s;
+  srv.request.i = req.i;
+  srv.request.precise = false;
+  if (client.call(srv))
+  {
+    prior_setting = srv.response.prior_setting;
+  }
+  else
+  {
+    ROS_ERROR("unexpected call fail to service when starting");
+    res.status = -3;
+    return true;
+  }
+
+  float diff_to_object;
+  static tf::TransformListener tl;
+  tf::StampedTransform base_to_object;
+  ros::Time now = ros::Time::now();
+  tl.waitForTransform("leg_base_link", req.object, now, ros::Duration(2.0));
+
+  // find current distance to target object
+  try
+  {
+    tl.lookupTransform("leg_base_link", req.object, now, base_to_object);
+    diff_to_object = base_to_object.getOrigin().x();
+  }
+  catch (std::exception e)
+  {
+    ROS_ERROR("failed tf listen during start of call");
+    res.status = -3;
+    return true;
+  }
+
+  // move robot forward
+  GoPos(RUN_GAIN, 0, 0);
+  float initial_diff = diff_to_object;
+
+  ROS_WARN("Going for target : %f", initial_diff);
+
+  Clock::time_point start = Clock::now();
+
+  // stop when the robot is close enough to target object
+  while (diff_to_object > GO_AS_NEAR_TO)
+  {
+    now = ros::Time::now();
+    tl.waitForTransform("leg_base_link", req.object, now, ros::Duration(2.0));
+
+    try
+    {
+      tl.lookupTransform("leg_base_link", req.object, now, base_to_object);
+      diff_to_object = base_to_object.getOrigin().x();    
+    }
+    catch (std::exception e)
+    {
+      ROS_ERROR("failed tf listen during run");
+      ac->cancelGoal();
+      res.status = -2;
+      return true;
+    }
+
+    // update search area
+    srv.request.x_cap = req.x_cap;
+    srv.request.y_cap = req.y_cap;
+    srv.request.z_cap = req.z_cap;
+    srv.request.x = req.x;
+    srv.request.y = req.y;
+    srv.request.z = req.z - (initial_diff - diff_to_object);
+    srv.request.h_cap = req.h_cap;
+    srv.request.s_cap = req.s_cap;
+    srv.request.i_cap = req.i_cap;
+    srv.request.h = req.h;
+    srv.request.s = req.s;
+    srv.request.i = req.i;
+    srv.request.precise = false;
+    if (client.call(srv))
+    {
+      if (srv.response.status <= 0)
+      {
+	ROS_ERROR("object lost");
+	ac->cancelGoal();
+	res.status = -2;
+	return true;
+      }
+    }
+    else
+    {
+      ROS_ERROR("unexpected call fail to service");
+      ac->cancelGoal();
+      res.status = -2;
+      return true;
+    }
+
+    ROS_WARN("running");
+    usleep(500 * 1000);
+
+    Clock::time_point time_now = Clock::now();
+    if (std::chrono::duration_cast<milliseconds>(time_now - start).count()
+	> GIVE_UP_TIME)
+    {
+      ROS_ERROR("too long of a run!");
+      ac->cancelGoal();
+      res.status = -4;
+      return true;
+    }
+  }
+
+  ac->cancelGoal();
+  res.status = 1;
+
+  ROS_WARN("finished");
+
+  Clock::time_point end = Clock::now();
+  res.time =
+      std::chrono::duration_cast<milliseconds>(end - start).count() / 1000.0;
+
+  // set settings back to original
+  srv.request.x_cap = req.x_cap;
+  srv.request.y_cap = req.y_cap;
+  srv.request.z_cap = req.z_cap;
+  srv.request.x = req.x;
+  srv.request.y = req.y;
+  srv.request.z = req.z - (initial_diff - diff_to_object);
+  srv.request.h_cap = req.h_cap;
+  srv.request.s_cap = req.s_cap;
+  srv.request.i_cap = req.i_cap;
+  srv.request.h = req.h;
+  srv.request.s = req.s;
+  srv.request.i = req.i;
+  srv.request.precise = prior_setting;
+  if (!client.call(srv))
+  {
+    ROS_ERROR("failed to set settings back to prior");
+    res.status = -1;
+  }
+
+  return true;
+};
+
+//////////////////////////////////////////////////
+bool GoForTime(aero_startup::Time::Request  &req,
+	       aero_startup::Time::Response &res)
+{
+  GoPos(RUN_GAIN, 0, 0);
+
+  usleep(req.time * 1000 * 1000);
+
+  ac->cancelGoal();
+  res.status = 1;
+
+  return true;
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "go_run");
+  ros::NodeHandle nh;
+
+  ac = new actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>(
+      "/move_base/goal", true);
+
+  ros::ServiceServer service_obj =
+      nh.advertiseService("/go_run/target_object", GoForTarget);
+  ros::ServiceServer service_tm =
+      nh.advertiseService("/go_run/target_time", GoForTime);
+
+  client = nh.serviceClient<aero_startup::PointXYZHSI>(
+      "/extract_object_features/perception_area");
+
+  ros::spin();
+
+  return 0;
+}
