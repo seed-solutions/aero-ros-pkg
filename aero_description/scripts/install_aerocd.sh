11e7f28d6920775ea8e09b5128fb1dd2553738fe
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2015-12-13 20:34:04 +0900

    added installation of aerocd

---
+++ aero_description/scripts/install_aerocd.sh
@@ -0,0 +1,25 @@
+echo -e "
+function aerocd() {
+    if [[ \$1 == \"\" ]]
+    then
+        roscd aero_startup
+        return
+    fi
+    num=\$(find \$(rospack find aero_startup)/ -name \"\$1*\" | wc -l)
+    if [[ \$num -eq 0 ]]
+    then
+        echo \"not found\"
+        return
+    fi
+    file=\$(find \$(rospack find aero_startup)/ -name \"\$1*\" | sed 's:/[^/]*\$::' | sort -u)
+    if [[ \"\$num\" -gt \"1\" ]]
+    then
+        if [[ \$2 == \"\" ]]
+        then
+            echo -e \"which?\\\n\${file}\"
+            return
+        fi
+        file=\$(find \$(rospack find aero_startup)/ -name \"\$1*\" | sed 's:/[^/]*\$::' | sort -u | sed -n \"\${2}p\")
+    fi
+    cd \$file
+}" >> $HOME/.bashrc
