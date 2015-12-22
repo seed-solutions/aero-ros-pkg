9f275354cf3128324adaa2b281ea1f9b89f9c867
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2015-12-22 21:49:39 +0900

    non-c++11 build now possible

--- aero_description/scripts/configure_applications.sh
+++ aero_description/scripts/configure_applications.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# prerequisites : {my_robot}/applications.cfg
+# prerequisites : ${app}.cfg
 
 # modifies  : aero_startup/CMakeLists.txt
+# generates : ${app}.launch
 
 app=$1
 robot=$2
@@ -10,7 +11,14 @@ robot=$2
 if [[ $robot == "" ]]
 then
     input_file=$(find $(rospack find aero_description)/ -name "${app}.cfg")
-    if [[ $(find $(rospack find aero_description)/ -name "${app}.cfg" | wc -l) -ne 1 ]]
+    if [[ $input_file == "" ]]
+    then # .cfg under aero_startup/sandbox is also valid
+	input_file=$(find $(rospack find aero_startup)/sandbox/ -name "${app}.cfg")
+	num_of_candidates=$(find $(rospack find aero_startup)/sandbox/ -name "${app}.cfg" | wc -l)
+    else
+	num_of_candidates=$(find $(rospack find aero_description)/ -name "${app}.cfg" | wc -l)
+    fi
+    if [[ $num_of_candidates -ne 1 ]]
     then
 	echo "aborting, robot not specified or file not found"
 	exit
@@ -25,6 +33,18 @@ launch_file="$(rospack find aero_description)/../aero_startup/${app}.launch"
 # create launch
 
 echo -e "<launch>\n</launch>" > $launch_file
+
+# delete dependencies in CMakeLists.txt
+
+delete_from_line=$(grep -n -m 1 ">>> add dependencies" $cmake_file | cut -d ':' -f1)
+delete_from_line=$(($delete_from_line + 1))
+delete_to_line=$(grep -n -m 1 "<<< add dependencies" $cmake_file | cut -d ':' -f1)
+
+if [[ $delete_to_line -ne $delete_from_line ]]
+then
+    delete_to_line=$(($delete_to_line - 1))
+    sed -i "${delete_from_line},${delete_to_line}d" $cmake_file
+fi
 
 # delete applications in CMakeLists.txt
 
@@ -43,11 +63,11 @@ tab2=$'  '
 create_srv_file() {
     lookup_file=$1
 
-    check_for_srv=$(grep "@define srv" $1)
+    check_for_srv=$(grep "@define srv" $lookup_file)
     if [[ "$check_for_srv" != "" ]]
     then
 	# check num of srvs
-	num_of_srvs=$(grep "@define srv" $1 | wc -l)
+	num_of_srvs=$(grep "@define srv" $lookup_file | wc -l)
 	# create srv file
 	for (( srv=1; srv<=${num_of_srvs}; srv++ ))
 	do
@@ -55,9 +75,9 @@ create_srv_file() {
 	    srv_file="$(rospack find aero_startup)/srv/${srv_name}.srv"
 	    if [[ "$num_of_srvs" -gt "1" ]]
 	    then
-		awk "/@define srv ${srv}/,/\*\//" $1 > $srv_file
+		awk "/@define srv ${srv}/,/\*\//" $lookup_file > $srv_file
 	    else
-		awk "/@define srv/,/\*\//" $1 > $srv_file
+		awk "/@define srv/,/\*\//" $lookup_file > $srv_file
 	    fi
 	    sed -i "/@define/d" $srv_file
 	    sed -i "/\*\//d" $srv_file
@@ -74,6 +94,202 @@ create_srv_file() {
 	done
     fi
 }
+
+find_source_code() {
+    executable_path=$1
+    source_name=$2
+
+    # rename source depending on camel letters
+    find_source=$(find ${executable_path} -name "${source_name}*" | grep -v ".hh")
+    has_same_name_class_object=$(find ${executable_path} -name "${source_name}*" | grep "class")
+    if [[ $find_source == "" ]]
+    then
+	num_of_words=$(echo $source_name | awk -F_ '{print NF}')
+	file_name=''
+	for (( num=1; num<=$num_of_words; num++ ))
+	do
+	    word=$(echo $source_name | awk -F_ '{print $'$num'}')
+	    file_name="${file_name}${word^}"
+	done
+	find_source=$(find ${executable_path} -name "${file_name}*" | grep -v ".hh")
+	has_same_name_class_object=$(find ${executable_path} -name "${file_name}*" | grep "/class/")
+    fi
+    # if class object with same name as executable exists
+    if [[ $has_same_name_class_object != "" ]]
+    then
+	find_source=$(find ${executable_path} -name "${file_name}*" | grep -v ".hh" | grep -v "/class/") 
+    fi
+    echo -e $find_source
+}
+
+parse_line() {
+    line=$1
+
+    header=$(echo $line | cut -d: -f1)
+    executable_name=$(echo $header | awk '{print $2}')
+    body=$(echo $line | cut -d: -f2)
+    executable_dir=$(echo $body | awk '{print $1}')
+    source_name=$(echo $executable_name)
+    executable_path="$(rospack find aero_description)/../aero_startup/${executable_dir}"
+    path_passed_source_name=$(find_source_code $executable_path $source_name)
+    source_name=$(echo "${path_passed_source_name}" | awk -F/ '{print $NF}')
+
+    # add executable to CMakeLists.txt
+    libs=''
+    ifs=''
+    # dependecy check
+    dependencies=$(echo $line | cut -d: -f3)
+    num_of_dependencies=$(echo $dependencies | awk '{print NF}')
+    dependency_list="$(rospack find aero_description)/../aero_startup/.dependencies"
+    for (( num=1; num<=${num_of_dependencies}; num++ ))
+    do
+	target=$(echo $dependencies | awk '{print $'$num'}')
+	libs="${libs} $(grep "${target}" $dependency_list | awk '{print $2}')"
+	if [[ $ifs == "" ]]
+	then
+	    ifs="FOUND_${target}"
+	else
+	    ifs="${ifs} AND FOUND_${target}"
+	fi
+    done
+    write_to_line=$(grep -n -m 1 ">>> add applications" $cmake_file | cut -d ':' -f1)
+    write_to_line=$(($write_to_line + 1))
+    tab=''
+    if [[ $ifs != "" ]]
+    then
+	tab=$'  '
+	echo "if(${ifs})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+	write_to_line=$(($write_to_line + 1))
+    fi
+
+    # add_executable
+    echo "${tab}add_executable(${executable_name}" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+    write_to_line=$(($write_to_line + 1))
+    includes_main=$(find $executable_path -name Main.cc 2>/dev/null)
+    include_class_objects=$(grep "/class/" $path_passed_source_name)
+    class_files=""
+    num_of_class_files=""
+    if [[ $includes_main != "" ]] # if executable is Main.cc
+    then
+	cc_files=$(find $executable_path -name "*.cc" | xargs -0 -I{} echo "{}" | awk -F/ '{print $NF}' | tr '\n' ' ')
+	num_of_cc_files=$(find $executable_path -name "*.cc" | wc -l)
+
+	for (( num=1; num<=${num_of_cc_files}; num++ ))
+	do
+	    # file=$(echo $cc_files | sed -n "${num}p")
+	    file=$(echo $cc_files | awk '{print $'$num'}')
+	    echo "${tab}${tab2}${executable_dir}/${file}" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+	    write_to_line=$(($write_to_line + 1))
+	done
+	echo "${tab}${tab2})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+	write_to_line=$(($write_to_line + 1))
+    elif [[ $include_class_objects != "" ]] # if relies on class objects
+    then
+	class_files=$(grep "#include \"aero_" $path_passed_source_name | grep "/class/" | awk '{print $2}' | cut -d\" -f2 | tr '\n' ' ')
+	num_of_class_files=$(grep "#include \"aero_" $path_passed_source_name | grep "class" | wc -l)
+	for (( num=1; num<=${num_of_class_files}; num++ ))
+	do
+	    # file=$(echo $class_files | sed -n "${num}p" | sed "s/.hh/.cc/g")
+	    file=$(echo $class_files | awk '{print $'$num'}' | sed "s/.hh/.cc/g")
+	    echo "${tab}${tab2}${file}" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+	    write_to_line=$(($write_to_line + 1))
+	done
+	echo "${tab}${tab2}${executable_dir}/${source_name})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+	write_to_line=$(($write_to_line + 1))
+    else
+	echo "${tab}${tab2}${executable_dir}/${source_name})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+	write_to_line=$(($write_to_line + 1))
+    fi
+
+    # target link
+    echo "${tab}target_link_libraries(${executable_name} \${catkin_LIBRARIES} \${Boost_LIBRARIES}" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+    write_to_line=$(($write_to_line + 1))
+    echo "${tab}${tab2}${libs})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+    if [[ $ifs != "" ]]
+    then
+	write_to_line=$(($write_to_line + 1))
+	echo "endif()\n" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+    else
+	write_to_line=$(($write_to_line + 1))
+	echo "" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+    fi
+
+    # add srv files if required
+    create_srv_file "${executable_path}/${source_name}"
+    if [[ $class_files != "" ]]
+    then
+	for (( num=1; num<=${num_of_class_files}; num++ ))
+	do
+	    # file=$(echo $class_files | sed -n "${num}p")
+	    file=$(echo $class_files | awk '{print $'$num'}')
+	    create_srv_file "$(rospack find aero_description)/../aero_startup/${file}"
+	done
+    fi
+}
+
+# handle non-c++11 build files first
+
+non_cpp_build=""
+while read line
+do
+    header=$(echo $line | cut -d: -f1)
+    proto=$(echo $header | awk '{print $1}')
+    if [[ $proto == "!" ]] # non-c++11 build
+    then
+	parse_line "$line"
+	non_cpp_build="true"
+    fi
+done < $input_file
+
+if [[ $non_cpp_build != "" ]]
+then
+    echo "building non-c++11 files"
+    where_i_was=$(pwd)
+    cd $(rospack find aero_description)
+    catkin b aero_startup
+fi
+
+# delete non-c++11 builds
+
+delete_from_line=$(grep -n -m 1 ">>> add applications" $cmake_file | cut -d ':' -f1)
+delete_from_line=$(($delete_from_line + 1))
+delete_to_line=$(grep -n -m 1 "<<< add applications" $cmake_file | cut -d ':' -f1)
+
+if [[ $delete_to_line -ne $delete_from_line ]]
+then
+    delete_to_line=$(($delete_to_line - 1))
+    sed -i "${delete_from_line},${delete_to_line}d" $cmake_file
+fi
+
+# add c++ dependencies :
+# include(CheckCXXCompilerFlag)
+# CHECK_CXX_COMPILER_FLAG("-std=c+11" COMPILER_SUPPORTS_CXX11)
+# CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
+# if(COMPILER_SUPPORTS_CXX11)
+#   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+#   add_definitions(-DCXX11_SUPPORTED)
+# elseif(COMPILER_SUPPORTS_CXX0X)
+#   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+#   add_definitions(-DCXX11_SUPPORTED)
+# else()
+#   message(FATAL "c++11 required but not supported")
+# endif()
+# end dep
+
+awk "/# add c\+\+ dependencies :/,/# end dep/" $(rospack find aero_description)/scripts/configure_applications.sh > /tmp/cxx11dep
+sed -i "/# add c\+\+/d" /tmp/cxx11dep
+sed -i "/# end dep/d" /tmp/cxx11dep
+sed -i "s/# //g" /tmp/cxx11dep
+write_to_line=$(grep -n -m 1 ">>> add dependencies" $cmake_file | cut -d ':' -f1)
+write_to_line=$(($write_to_line + 1))
+IFS=''
+while read line
+do
+    echo "${line}" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
+    write_to_line=$(($write_to_line + 1))
+done < /tmp/cxx11dep
+
+# .cfg main parsing starts here
 
 executable_name=""
 node_args=0
@@ -106,29 +322,6 @@ do
     executable_name=$(echo $header | awk '{print $2}')
     body=$(echo $line | cut -d: -f2)
     executable_dir=$(echo $body | awk '{print $1}')
-    source=$(echo $executable_name)
-    executable_path="$(rospack find aero_description)/../aero_startup/${executable_dir}"
-    # rename source depending on camel letters
-    find_source=$(find ${executable_path} -name "${source}*")
-    has_same_name_class_object=$(find ${executable_path} -name "${source}*" | grep "class")
-    if [[ $find_source == "" ]]
-    then
-	num_of_words=$(echo $source | awk -F_ '{print NF}')
-	file_name=''
-	for (( num=1; num<=$num_of_words; num++ ))
-	do
-	    word=$(echo $source | awk -F_ '{print $'$num'}')
-	    file_name="${file_name}${word^}"
-	done
-	find_source=$(find ${executable_path} -name "${file_name}*")
-	has_same_name_class_object=$(find ${executable_path} -name "${file_name}*" | grep "/class/")
-    fi
-    # if class object with same name as executable exists
-    if [[ $has_same_name_class_object != "" ]]
-    then
-	find_source=$(find ${executable_path} -name "${file_name}*" | grep -v "/class/") 
-    fi
-    source=$(echo "${find_source}" | awk -F/ '{print $NF}')
 
     # add to launch
     tab6=$'      '
@@ -145,7 +338,11 @@ do
     if [[ $proto == "&" ]] # requires special installation script
     then
 	install=$(echo $line | cut -d: -f3)
-	${executable_path}/.install $install
+	if [[ $install == " this" ]]
+	then
+	    install=$(echo "$install" | sed "s@ this@${executable_name}@")
+	fi
+	$(rospack find aero_description)/../aero_startup/${executable_dir}/.install $install
 
     elif [[ $proto == "+" ]]
     then
@@ -153,94 +350,11 @@ do
 	then
 	    continue
 	fi
-
-	# add executable to CMakeLists.txt
-	libs=''
-	ifs=''
-
-	# dependecy check
-	dependencies=$(echo $line | cut -d: -f3)
-	num_of_dependencies=$(echo $dependencies | awk '{print NF}')
-	dependency_list="$(rospack find aero_description)/../aero_startup/.dependencies"
-	for (( num=1; num<=${num_of_dependencies}; num++ ))
-	do
-	    target=$(echo $dependencies | awk '{print $'$num'}')
-	    libs="${libs} $(grep "${target}" $dependency_list | awk '{print $2}')"
-	    if [[ $ifs == "" ]]
-	    then
-		ifs="FOUND_${target}"
-	    else
-		ifs="${ifs} AND FOUND_${target}"
-	    fi
-	done
-	write_to_line=$(grep -n -m 1 ">>> add applications" $cmake_file | cut -d ':' -f1)
-	write_to_line=$(($write_to_line + 1))
-	tab=''
-	if [[ $ifs != "" ]]
-	then
-	    tab=$'  '
-	    echo "if(${ifs})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
-	    write_to_line=$(($write_to_line + 1))
-	fi
-
-	# add_executable
-	echo "${tab}add_executable(${executable_name}" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
-	write_to_line=$(($write_to_line + 1))
-	includes_main=$(find $executable_dir -name Main.cc 2>/dev/null)
-	include_class_objects=$(grep "/class/" $find_source)
-	class_files=""
-	num_of_class_files=""
-	if [[ $includes_main != "" ]] # if executable is Main.cc
-	then
-	    cc_files=$(find $copy_to_dir -name "*.cc" | xargs -0 -I{} echo "{}" | awk -F/ '{print $NF}')
-	    num_of_cc_files=$(find $copy_to_dir -name "*.cc" | wc -l)
-	    for (( num=1; num<=${num_of_cc_files}; num++ ))
-	    do
-		file=$(echo $cc_files | awk '{print $'$num'}')
-		echo "${tab}${tab2}${executable_dir}/${file}" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
-		write_to_line=$(($write_to_line + 1))
-	    done
-	    echo "${tab}${tab2})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
-	    write_to_line=$(($write_to_line + 1))
-	elif [[ $include_class_objects != "" ]] # if relies on class objects
-	then
-	    class_files=$(grep "#include \"aero_" $find_source | grep "/class/" | awk '{print $2}' | cut -d\" -f2)
-	    num_of_class_files=$(grep "#include \"aero_" $find_source | grep "class" | wc -l)
-	    for (( num=1; num<=${num_of_class_files}; num++ ))
-	    do
-		file=$(echo $class_files | awk '{print $'$num'}' | sed "s/.hh/.cc/g")
-		echo "${tab}${tab2}${file}" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
-		write_to_line=$(($write_to_line + 1))
-	    done
-	    echo "${tab}${tab2}${executable_dir}/${source})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
-	    write_to_line=$(($write_to_line + 1))
-	else
-	    echo "${tab}${tab2}${executable_dir}/${source})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
-	    write_to_line=$(($write_to_line + 1))
-	fi
-
-	# target link
-	echo "${tab}target_link_libraries(${executable_name} \${catkin_LIBRARIES} \${Boost_LIBRARIES}" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
-	write_to_line=$(($write_to_line + 1))
-	echo "${tab}${tab2}${libs})" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
-	if [[ $ifs != "" ]]
-	then
-	    write_to_line=$(($write_to_line + 1))
-	    echo "endif()\n" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
-	else
-	    write_to_line=$(($write_to_line + 1))
-	    echo "" | xargs -0 -I{} sed -i "${write_to_line}i\{}" $cmake_file
-	fi
-
-	# add srv files if required
-	create_srv_file "${executable_path}/${source}"
-	if [[ $class_files != "" ]]
-	then
-	    for (( num=1; num<=${num_of_class_files}; num++ ))
-	    do
-		file=$(echo $class_files | awk '{print $'$num'}')
-		create_srv_file "$(rospack find aero_description)/../aero_startup/${file}"
-	    done
-	fi
+	parse_line "$line"
     fi
 done < $input_file
+
+echo "building c++11 files"
+cd $(rospack find aero_description)
+catkin b aero_startup
+cd $where_i_was
