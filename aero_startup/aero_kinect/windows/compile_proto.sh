d2b10a57244eca31b15f0ecfaa0e003d669cd677
Hiroaki Yaguchi <h-yaguchi@jsk.t.u-tokyo.ac.jp>
2016-04-07 22:20:45 +0900

    move aero_kinect from aero_startup to aero_application

--- aero_startup/aero_kinect/windows/compile_proto.sh
+++
@@ -1,3 +0,0 @@
-protoc_exe=$(find $HOME/ -name protoc.exe | grep -m 1 "third_party/protobuf/cmake/Release/")
-grpc_exe=$(echo "$protoc_exe" | awk -F/ '{sub("/"$NF,"");print}')/../../../../examples/csharp/helloworld/packages/Grpc.Tools.0.7.1/tools/grpc_csharp_plugin.exe
-$protoc_exe -I $1/protos --csharp_out $1/$1 --grpc_out $1/$1 --plugin=protoc-gen-grpc=$grpc_exe $1/protos/$2
