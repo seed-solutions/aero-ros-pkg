d99b3ca44f2994b9f7370ef4c93d7fdd13228372
Hiroaki Yaguchi <h-yaguchi@jsk.t.u-tokyo.ac.jp>
2016-06-15 16:59:12 +0900

    add comments

--- aero_startup/aero_move_base/AeroMoveBase.cc
+++ aero_startup/aero_move_base/AeroMoveBase.cc
@@ -17,19 +17,19 @@ AeroMoveBase::AeroMoveBase(const ros::NodeHandle& _nh) :
   states_.cur_vel.resize(num_of_wheels_);
 
   wheel_pub_ =
-    nh_.advertise<trajectory_msgs::JointTrajectory>(
-	 "/aero_controller/wheel_command", 10);
+      nh_.advertise<trajectory_msgs::JointTrajectory>(
+          "/aero_controller/wheel_command", 10);
 
   servo_pub_ =
-    nh_.advertise<std_msgs::Bool>("/aero_controller/wheel_servo", 10);
+      nh_.advertise<std_msgs::Bool>("/aero_controller/wheel_servo", 10);
 
   simple_goal_sub_ =
-    nh_.subscribe("move_base_simple/goal",
-		  10, &AeroMoveBase::SetSimpleGoal, this);
+      nh_.subscribe("move_base_simple/goal",
+                    10, &AeroMoveBase::SetSimpleGoal, this);
 
   timer_ =
-    nh_.createTimer(ros::Duration(ros_rate_),
-		    &AeroMoveBase::MoveBase, this);
+      nh_.createTimer(ros::Duration(ros_rate_),
+                      &AeroMoveBase::MoveBase, this);
 
   as_.registerGoalCallback(boost::bind(&AeroMoveBase::SetActionGoal, this));
   as_.registerPreemptCallback(boost::bind(&AeroMoveBase::CancelGoal, this));
@@ -89,16 +89,16 @@ bool AeroMoveBase::MoveBaseOnce()
     {
       states_.cur_vel[i] += goal_.wheel_dV[i];
       if (fabs(states_.cur_vel[i]) > fabs(goal_.max_vel[i]))
-	states_.cur_vel[i] = goal_.max_vel[i];
+        states_.cur_vel[i] = goal_.max_vel[i];
     }
   else if (states_.cur_time >= (goal_.run_time - goal_.warm_up_time))
     for (unsigned int i = 0; i < goal_.wheel_dV.size(); ++i)
     {
       states_.cur_vel[i] -= goal_.wheel_dV[i];
       if (goal_.wheel_dV[i] > 0 && states_.cur_vel[i] < 0)
-	states_.cur_vel[i] = 0.0;
+        states_.cur_vel[i] = 0.0;
       if (goal_.wheel_dV[i] < 0 && states_.cur_vel[i] > 0)
-	states_.cur_vel[i] = 0.0;
+        states_.cur_vel[i] = 0.0;
     }
   else
     for (unsigned int i = 0; i < num_of_wheels_; ++i)
@@ -131,17 +131,20 @@ void AeroMoveBase::SetGoal(float _x, float _y, float _theta)
   wheels wheel_data = this->Translate(_x, _y, _theta);
 
   // set goals
-
   goal_.run_time = wheel_data.time;
 
   goal_.warm_up_time = 1.0;
-  if (goal_.run_time < 2.0) // if move within two seconds
+  if (goal_.run_time < 2.0)  // if move within two seconds
     goal_.warm_up_time =
-      static_cast<int>(goal_.run_time * 0.5 / ros_rate_) * ros_rate_;
-
-  int steps = goal_.warm_up_time / ros_rate_;
-  if (steps == 0) steps = 1; // no warm up time
-  else goal_.run_time += goal_.warm_up_time; // 1 sec delay because of warm up
+        static_cast<float>(
+            static_cast<int>(goal_.run_time * 0.5 / ros_rate_)
+            * ros_rate_);
+  // steps means how many steps MoveBaseOnce will run in warm up
+  int steps = static_cast<int>(goal_.warm_up_time / ros_rate_);
+  // if steps == 0, set 1 step and no warm up time
+  if (steps == 0) steps = 1;
+  // or add warm_up_time sec delay because of warm up
+  else goal_.run_time += goal_.warm_up_time;
 
   for (unsigned int i = 0; i < wheel_data.velocities.size(); ++i)
   {
@@ -150,18 +153,16 @@ void AeroMoveBase::SetGoal(float _x, float _y, float _theta)
   }
 
   // set states
-
   std::fill(states_.cur_vel.begin(), states_.cur_vel.end(), 0.0);
   states_.cur_time = 0.0;
   states_.moved_distance = {0.0, 0.0, 0.0};
 
   // start wheels
-
   std_msgs::Bool dat;
   dat.data = true;
   servo_pub_.publish(dat);
 
-  usleep(1000 * 300);
+  usleep(1000 * 300);  // sleep until servo_on finished
 
   states_.wheel_on = true;
 }
@@ -208,24 +209,26 @@ void AeroMoveBase::SetSimpleGoal(
 /// @brief stop and servo off all wheels
 void AeroMoveBase::FinishMove()
 {
-    // stop wheels
-    std::vector<double> zeros(num_of_wheels_, 0.0);
+  // stop wheels
+  // send zero for all wheels
+  std::vector<double> zeros(num_of_wheels_, 0.0);
 
-    trajectory_msgs::JointTrajectory msg;
-    msg.joint_names = wheel_names_;
-    msg.points.resize(1);
-    msg.points[0].positions = zeros;
-    msg.points[0].time_from_start = ros::Duration(ros_rate_);
-    wheel_pub_.publish(msg);
+  trajectory_msgs::JointTrajectory msg;
+  msg.joint_names = wheel_names_;
+  msg.points.resize(1);
+  msg.points[0].positions = zeros;
+  msg.points[0].time_from_start = ros::Duration(ros_rate_);
+  wheel_pub_.publish(msg);
 
-    usleep(1000 * 300);
+  usleep(1000 * 300);  // sleep until wheel command finished
 
-    std_msgs::Bool dat;
-    dat.data = false;
-    servo_pub_.publish(dat);
+  // wheel_off
+  std_msgs::Bool dat;
+  dat.data = false;
+  servo_pub_.publish(dat);
 
-    states_.wheel_on = false;
+  states_.wheel_on = false;
 
-    // reset goals and states in next goal call
-    // does not reset here for future references
+  // reset goals and states in next goal call
+  // does not reset here for future references
 }
