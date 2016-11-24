2a4c2ef56fb6737c7afa6d40ccef2e5be02c6036
Shintaro Hori <hori@jsk.imi.i.u-tokyo.ac.jp>
2016-11-24 17:27:43 +0900

    smooth trajectory

--- aero_moveit_config/scripts/joint_trajectory_action_server.py
+++ aero_moveit_config/scripts/joint_trajectory_action_server.py
@@ -59,7 +59,9 @@ class FollowAction(object):
         print "execute" + rospy.get_name()
         self._finishable = False
         command = JointTrajectory()
-        command = goal.trajectory
+        command.header = goal.trajectory.header
+        command.joint_names = goal.trajectory.joint_names
+        command.points = goal.trajectory.points[-1]
         self._active_goal = goal
         self._active_command = command
         self._active_end_time = rospy.get_rostime() + goal.trajectory.points[-1].time_from_start + goal.goal_time_tolerance
