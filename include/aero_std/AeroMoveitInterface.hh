5b07eec3a91a58a091efcb48bdff63c40559bac3
Shintaro Hori <hori@jsk.imi.i.u-tokyo.ac.jp>
2017-01-20 17:47:46 +0900

    dexygen comment for AeroMoveitInterface Constructor

--- include/aero_std/AeroMoveitInterface.hh
+++ include/aero_std/AeroMoveitInterface.hh
@@ -26,6 +26,10 @@ namespace aero
   {
   public:
     //explicit AeroMoveitInterface();
+
+    /// @brief constructor
+    /// @param _nh ros node handler
+    /// @param _rd robot_description's name, "_rd", "_rd_ho" and "_rd_op" will be loaded
     explicit AeroMoveitInterface(ros::NodeHandle _nh, std::string _rd);
     ~AeroMoveitInterface();
     
