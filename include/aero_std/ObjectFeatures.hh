4faa335138c0034d7c186140e68d69933392c694
Shintaro Hori <hori@jsk.imi.i.u-tokyo.ac.jp>
2017-02-20 20:24:33 +0900

    set marker function's prototype

--- include/aero_std/ObjectFeatures.hh
+++ include/aero_std/ObjectFeatures.hh
@@ -3,6 +3,7 @@
 
 #include <ros/ros.h>
 #include <aero_std/AeroMoveitInterface.hh>
+#include <visualization_msgs/Marker.h>
 
 namespace aero
 {
@@ -11,25 +12,26 @@ namespace aero
 
     class ObjectFeatures
     {
-    public: explicit ObjectFeatures(ros::NodeHandle _nh);
+    public: explicit ObjectFeatures(ros::NodeHandle _nh, aero::interface::AeroMoveitInterfacePtr _interface);
 
     public: ~ObjectFeatures();
 
       // Convert vectors in camera coords to world coords.
-    public: Eigen::Vector3d ConvertWorld(Eigen::Vector3d _pos_camera);
+    public: Eigen::Vector3d ConvertWorld(Eigen::Vector3d _pos);
 
-    public: void BroadcastTf(Eigen::Vector3f _position, std::string _name);
+    public: void setCameraTransform(std::string _camera_parent_link, Eigen::Vector3d _position, Eigen::Quaterniond _orientation);
 
-    public: void BroadcastTf(Eigen::Vector3f _position,
-                             Eigen::Quaternionf _orientation,
-                             std::string _name);
-
-    public: void StopBroadcastTfAll();
-
+    public: int setMarker(geometry_msgs::Point _position, int _id=1);
 
     private: ros::NodeHandle nh_;
 
     private: aero::interface::AeroMoveitInterfacePtr interface_;
+
+    private: ros::Publisher marker_publisher_;
+
+    private: std::string camera_parent_link_;
+    private: Eigen::Vector3d camera_relative_position_;
+    private: Eigen::Quaterniond camera_relative_orientation_;
     };
 
     typedef std::shared_ptr<ObjectFeatures> ObjectFeaturesPtr;
--- src/ObjectFeatures.cc
+++ src/ObjectFeatures.cc
@@ -1,13 +1,17 @@
-#include "ObjectFeatures.hh"
+#include "aero_std/ObjectFeatures.hh"
 
 using namespace aero;
 using namespace vision;
 
 //////////////////////////////////////////////////
-ObjectFeatures::ObjectFeatures(ros::NodeHandle _nh, aero::interface::AeroMoveitInterfacePtr interface_ptr)
+ObjectFeatures::ObjectFeatures(ros::NodeHandle _nh, aero::interface::AeroMoveitInterfacePtr _interface)
   : nh_(_nh)
 {
-  interface_ = interface_ptr;
+  interface_ = _interface;
+
+  marker_publisher_ = nh_.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  camera_relative_position_ = Eigen::Vector3d(0.0, 0.0, 0.0);
+  camera_relative_orientation_ = Eigen::Quaterniond(1.0, 0.0, 0.0, 0.0);
 }
 
 //////////////////////////////////////////////////
@@ -16,16 +20,31 @@ ObjectFeatures::~ObjectFeatures()
 }
 
 //////////////////////////////////////////////////
-Eigen::Vector3f ObjectFeatures::ConvertWorld(Eigen::Vector3f _pos_camera)
+Eigen::Vector3d ObjectFeatures::ConvertWorld(Eigen::Vector3d _pos)
 {
-  Eigen::Quaternionf base_to_eye_q =
-    Eigen::Quaternionf(base_to_eye_.orientation.x,
-		       base_to_eye_.orientation.y,
-		       base_to_eye_.orientation.z,
-		       base_to_eye_.orientation.w);
 
-  return
-    Eigen::Vector3f(base_to_eye_.position.x,
-		    base_to_eye_.position.y,
-		    base_to_eye_.position.z) + base_to_eye_q * _pos_camera;
+  interface_->setRobotStateToCurrentState();
+
+  Eigen::Vector3d p_pos = interface_->kinematic_state->getGlobalLinkTransform(camera_parent_link_).translation();
+
+  Eigen::Matrix3d mat = interface_->kinematic_state->getGlobalLinkTransform(camera_parent_link_).rotation();
+  Eigen::Quaterniond p_qua(mat);
+
+  Eigen::Vector3d pos_tmp = camera_relative_orientation_ * _pos;
+
+  return p_pos + p_qua * camera_relative_position_ + p_qua * pos_tmp;
+    //    p_pos + p_qua * camera_relative_position_ + p_qua * camera_relative_position_ * _pos;
+}
+
+//////////////////////////////////////////////////
+void ObjectFeatures::setCameraTransform(std::string _camera_parent_link, Eigen::Vector3d _position, Eigen::Quaterniond _orientation)
+{
+  camera_parent_link_ = _camera_parent_link;
+  camera_relative_position_ = _position;
+  camera_relative_orientation_ = _orientation;
+}
+
+//////////////////////////////////////////////////
+int ObjectFeatures::setMarker(geometry_msgs::Point _position, int _id){
+  return _id;
 }
