b93169d9d59481feea7727af2a4292d2d09c05d2
Shintaro Hori <hori@jsk.imi.i.u-tokyo.ac.jp>
2017-03-21 16:46:57 +0900

    tmp length

--- aero_std/src/AeroMoveitInterface.cc
+++ aero_std/src/AeroMoveitInterface.cc
@@ -91,8 +91,10 @@ aero::interface::AeroMoveitInterface::AeroMoveitInterface(ros::NodeHandle _nh, s
   trajectory_groups_ = std::vector<std::string>();
   joint_states_ = sensor_msgs::JointState();
 
-  lifter_thigh_link_ = 0.250;
-  lifter_foreleg_link_ = 0.250;
+  // lifter_thigh_link_ = 0.250;
+  // lifter_foreleg_link_ = 0.250;
+  lifter_thigh_link_ = 0.235;
+  lifter_foreleg_link_ = 0.235;
 
   detected_speech_ = "";
 
