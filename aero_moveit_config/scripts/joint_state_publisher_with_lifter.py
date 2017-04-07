32fb89eb47f1b38d500090281034a95b1f9aea1b
Shintaro Hori <hori@jsk.imi.i.u-tokyo.ac.jp>
2017-04-07 17:29:30 +0900

    trick joint_state from lifter angle to lifte position

--- aero_moveit_config/scripts/joint_state_publisher_with_lifter.py
+++ aero_moveit_config/scripts/joint_state_publisher_with_lifter.py
@@ -16,13 +16,20 @@ class LifterStatePublisher(object):
         msg = JointState()
         msg.header = state.header
         msg.name = state.name
-        msg.name.extend(["virtual_lifter_z_joint", "virtual_lifter_x_joint"])
+        hip_id = state.name.index("hip_joint")
+        knee_id = state.name.index("knee_joint")
+        ankle_id = state.name.index("ankle_joint")
+        msg.name[hip_id] = "virtual_lifter_x_joint"
+        msg.name[knee_id] = "virtual_lifter_z_joint"
+        del msg.name[ankle_id]
         msg.position = list(state.position)
         hip = state.position[state.name.index("hip_joint")]
         knee = state.position[state.name.index("knee_joint")] 
         z = self._l1 * (math.cos(knee - hip) - 1.0) + self._l2 * (math.cos(hip) - 1.0)
         x = -self._l1 * math.sin(knee - hip) + self._l2 * math.sin(hip)
-        msg.position.extend([z*0.001, x*0.001])
+        msg.position[hip_id] = x*0.001
+        msg.position[knee_id] = z*0.001
+        del msg.position[ankle_id]
         self._pub.publish(msg)
         
 if __name__ == '__main__':
