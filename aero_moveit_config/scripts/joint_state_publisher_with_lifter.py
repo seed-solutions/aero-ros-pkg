8417ae685338ec118e8fb5a2f82734e44e03d621
Shintaro Hori <hori@jsk.imi.i.u-tokyo.ac.jp>
2017-04-07 18:04:41 +0900

    fix lifter postion publisher

--- aero_moveit_config/scripts/joint_state_publisher_with_lifter.py
+++ aero_moveit_config/scripts/joint_state_publisher_with_lifter.py
@@ -19,14 +19,14 @@ class LifterStatePublisher(object):
         hip_id = state.name.index("hip_joint")
         knee_id = state.name.index("knee_joint")
         ankle_id = state.name.index("ankle_joint")
-        msg.name[hip_id] = "virtual_lifter_x_joint"
-        msg.name[knee_id] = "virtual_lifter_z_joint"
-        del msg.name[ankle_id]
         msg.position = list(state.position)
         hip = state.position[state.name.index("hip_joint")]
         knee = state.position[state.name.index("knee_joint")] 
         z = self._l1 * (math.cos(knee - hip) - 1.0) + self._l2 * (math.cos(hip) - 1.0)
         x = -self._l1 * math.sin(knee - hip) + self._l2 * math.sin(hip)
+        msg.name[hip_id] = "virtual_lifter_x_joint"
+        msg.name[knee_id] = "virtual_lifter_z_joint"
+        del msg.name[ankle_id]
         msg.position[hip_id] = x*0.001
         msg.position[knee_id] = z*0.001
         del msg.position[ankle_id]
