d362a3fd0c9e2ceb02347ea82be423c76931490c
Shintaro Hori <hori@jsk.imi.i.u-tokyo.ac.jp>
2017-04-10 16:57:04 +0900

    add some lifter meshes

--- aero_description/scripts/create_urdf.sh
+++ aero_description/scripts/create_urdf.sh
@@ -12,6 +12,21 @@ lower_file=$(rospack find aero_description)/${dir}/$3.txt
 cp ${upper_file} /tmp/aero_ros_order_upper
 cp ${lower_file} /tmp/aero_ros_order_lower
 
+if [[ -d $(rospack find aero_description)/urdf ]]
+then
+    rm -rf $(rospack find aero_description)/urdf
+fi
+
+if [[ -d $(rospack find aero_description)/robots ]]
+then
+    rm -rf $(rospack find aero_description)/robots
+fi
+
+if [[ -d $(rospack find aero_description)/meshes ]]
+then
+    rm -rf $(rospack find aero_description)/meshes
+fi
+
 cp -r ${dir}/urdf $(rospack find aero_description)
 cp -r ${dir}/robots $(rospack find aero_description)
 cp -r ${dir}/meshes $(rospack find aero_description)
