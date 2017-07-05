e7d2683de8488b0e9f2f491fefff43f52fbec48b
Shintaro Hori <hori@jsk.imi.i.u-tokyo.ac.jp>
2017-07-05 17:31:56 +0900

    lifter length fixed from 0.235 to 0.25

--- aero_description/typeF/controllers/AeroTorsoController.cc
+++ aero_description/typeF/controllers/AeroTorsoController.cc
@@ -5,11 +5,11 @@
 
 //static const double link1_length = 250.0;
 //static const double link2_length = 250.0;
-static const double link1_length = 235.0;
-static const double link2_length = 235.0;
+static const double link1_length = 250.0;
+static const double link2_length = 250.0;
 static const double x_origin = 0.0;
 //static const double z_origin = 500.0;
-static const double z_origin = 470.0;
+static const double z_origin = 500.0;
 static const double stroke_per_sec = 10.0; // mm/sec
 
 ros::Publisher pub;
