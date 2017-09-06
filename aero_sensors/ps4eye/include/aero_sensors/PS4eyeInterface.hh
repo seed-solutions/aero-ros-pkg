9237a991be94caf36cd465573624ba02ac8974f8
Shintaro Hori <hori@jsk.imi.i.u-tokyo.ac.jp>
2017-09-06 15:29:09 +0900

    add ps4eye interface to aero_sensors

---
+++ aero_sensors/ps4eye/include/aero_sensors/PS4eyeInterface.hh
@@ -0,0 +1,95 @@
+#ifndef _AERO_SENSORS_PS4EYE_INTERFACE_
+#define _AERO_SENSORS_PS4EYE_INTERFACE_
+
+#include <ros/ros.h>
+
+#include <ros/callback_queue.h>
+#include <ros/subscribe_options.h>
+
+#include "sensor_msgs/PointCloud2.h"
+#include "sensor_msgs/Image.h"
+#include "sensor_msgs/RegionOfInterest.h"
+#include "geometry_msgs/Point.h"
+
+#include <mutex>
+#include <cstring>
+#include <cstdint>
+
+#include <limits>
+#include <cmath>
+
+namespace ps4eye
+{
+  namespace interface
+  {
+
+    class PS4eyeInterface
+    {
+    public: explicit PS4eyeInterface(ros::NodeHandle _nh);
+
+    public: ~PS4eyeInterface();
+
+    public: sensor_msgs::PointCloud2 ReadPoints();
+
+    public: sensor_msgs::PointCloud2 ReadPoints(float _scale_x, float _scale_y);
+
+    public: sensor_msgs::Image ReadImage();
+
+    public: sensor_msgs::PointCloud2 ReadPointsAfter(float _scale_x, float _scale_y);
+
+    public: sensor_msgs::Image ReadImageAfter();
+
+    public: std::vector<sensor_msgs::RegionOfInterest> ImageBounds
+    (std::vector<std::array<int, 4> > _depth_indicies);
+
+    public: std::vector<sensor_msgs::RegionOfInterest> ImageBounds
+    (std::vector<std::array<int, 4> > _depth_indicies, float _w_scale, float _h_scale);
+
+    public: std::vector<geometry_msgs::Point> ImageCenters
+    (std::vector<sensor_msgs::RegionOfInterest> _image_bounds);
+
+    public: void SetNow();
+
+    private: void DepthCallback(const sensor_msgs::PointCloud2::ConstPtr& _msg);
+
+    private: void ImageCallback(const sensor_msgs::Image::ConstPtr& _msg);
+
+    private: sensor_msgs::PointCloud2 depth_;
+
+    private: std::mutex depth_mutex_;
+
+    private: sensor_msgs::Image image_;
+
+    private: std::mutex image_mutex_;
+
+    private: ros::NodeHandle nh_;
+
+    private: ros::Subscriber depth_sub_;
+
+    private: ros::SubscribeOptions depth_ops_;
+
+    private: ros::CallbackQueue depth_queue_;
+
+    private: ros::AsyncSpinner depth_spinner_;
+
+    private: ros::Subscriber image_sub_;
+
+    private: ros::SubscribeOptions image_ops_;
+
+    private: ros::CallbackQueue image_queue_;
+
+    private: ros::AsyncSpinner image_spinner_;
+
+    private: int depth_width_;
+
+    private: int depth_height_;
+
+    private: ros::Time time_now_;
+    };
+
+    typedef std::shared_ptr<PS4eyeInterface> PS4eyeInterfacePtr;
+
+  }
+}
+
+#endif
