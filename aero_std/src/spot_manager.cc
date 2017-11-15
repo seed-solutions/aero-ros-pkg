ef573dcc0e7aeab3acb8f0c36ce2d2705383c72b
Hiroaki Yaguchi <h-yaguchi@jsk.t.u-tokyo.ac.jp>
2017-11-15 22:25:40 +0900

    split spot manager

--- aero_std/src/spot_manager.cc
+++ aero_std/src/spot_manager.cc
@@ -8,17 +8,14 @@
 
 #include <yaml-cpp/yaml.h>
 
+#include <aero_std/spot_list.hh>
+
 #include <aero_std/SaveSpot.h>
 #include <aero_std/GetSpot.h>
 #include <aero_std/DeleteSpot.h>
 #include <aero_std/GetSpots.h>
 
 namespace aero {
-struct Spot {
-  std::string name;
-  geometry_msgs::Pose pose;
-};
-
 class SpotManager {
  public:
   SpotManager(ros::NodeHandle _nh, std::string _file);
@@ -35,12 +32,7 @@ class SpotManager {
   void BroadcastSpots(const ros::TimerEvent& ev);
 
  private:
-  int GetIndex_(std::string _name);
-  std::vector<std::string> GetList_();
-
-  void ReadFromFile_();
-  void WriteIntoFile_();
-  std::vector<Spot> spots_;
+  SpotList spot_list_;
 
   ros::NodeHandle nh_;
   tf::TransformListener listener_;
@@ -60,7 +52,7 @@ class SpotManager {
 SpotManager::SpotManager(ros::NodeHandle _nh, std::string _file)
   : nh_(_nh),
     file_(_file) {
-  ReadFromFile_();
+  spot_list_.ReadFromFile(file_);
   save_spot_ = nh_.advertiseService("save_spot",
                                     &SpotManager::SaveSpot,
                                     this);
@@ -94,18 +86,7 @@ bool SpotManager::SaveSpot(aero_std::SaveSpot::Request & req,
     return false;
   }
 
-  int index = GetIndex_(req.name);
-
-  if (index < 0) {
-    ROS_INFO("add new location : %s", req.name.c_str());
-    Spot new_spot;
-    index = spots_.size();
-    spots_.push_back(new_spot);
-  } else {
-    ROS_INFO("renew location : %s", req.name.c_str());
-  }
-
-  Spot& spot = spots_[index];
+  Spot spot;
   spot.name = req.name;
   spot.pose.position.x = tr.getOrigin().x();
   spot.pose.position.y = tr.getOrigin().y();
@@ -114,7 +95,9 @@ bool SpotManager::SaveSpot(aero_std::SaveSpot::Request & req,
   spot.pose.orientation.y = tr.getRotation().y();
   spot.pose.orientation.z = tr.getRotation().z();
   spot.pose.orientation.w = tr.getRotation().w();
-  WriteIntoFile_();
+
+  spot_list_.SaveSpot(spot);
+  spot_list_.WriteIntoFile(file_);
 
   res.status = true;
   return true;
@@ -125,7 +108,7 @@ bool SpotManager::SaveSpot(aero_std::SaveSpot::Request & req,
 /// @param res spot pose (res.pose)
 bool SpotManager::GetSpot(aero_std::GetSpot::Request & req,
                           aero_std::GetSpot::Response& res) {
-  int index = GetIndex_(req.name);
+  int index = spot_list_.GetIndex(req.name);
 
   if (index < 0) {
     res.status = false;
@@ -134,7 +117,7 @@ bool SpotManager::GetSpot(aero_std::GetSpot::Request & req,
   }
 
   res.status = true;
-  res.pose = spots_[index].pose;
+  res.pose = spot_list_.spots()[index].pose;
 
   return true;
 }
@@ -144,25 +127,15 @@ bool SpotManager::GetSpot(aero_std::GetSpot::Request & req,
 /// @param res return status
 bool SpotManager::DeleteSpot(aero_std::DeleteSpot::Request & req,
                              aero_std::DeleteSpot::Response& res) {
-  int index = GetIndex_(req.name);
+  bool result = spot_list_.DeleteSpot(req.name);
 
-  if (index < 0) {
+  if (!result) {
     ROS_WARN("spot: %s doesn't exist", req.name.c_str());
     res.status = false;
     return false;
   }
 
-  std::vector<Spot> prev_spots;
-  prev_spots.assign(spots_.begin(), spots_.end());
-  spots_.clear();
-
-  for (int i = 0; i < prev_spots.size(); ++i) {
-    if (i != index) {
-      spots_.push_back(prev_spots[i]);
-    }
-  }
-
-  WriteIntoFile_();
+  spot_list_.WriteIntoFile(file_);
 
   ROS_INFO("spot: %s is successfully deleted", req.name.c_str());
   res.status = true;
@@ -174,86 +147,15 @@ bool SpotManager::DeleteSpot(aero_std::DeleteSpot::Request & req,
 /// @param res spot name list (res.spots)
 bool SpotManager::GetSpots(aero_std::GetSpots::Request & req,
                            aero_std::GetSpots::Response& res) {
-  std::vector<std::string> list = GetList_();
+  std::vector<std::string> list = spot_list_.GetList();
   for (auto it: list) res.spots.push_back(it);
   return true;
 }
 
-/// @brief read spots from yaml file
-void SpotManager::ReadFromFile_() {
-  spots_.clear();
-  std::ifstream ifs(file_);
-
-  if (ifs.is_open()) {
-    YAML::Node config = YAML::LoadFile(file_);
-    for (size_t i = 0; i < config.size(); ++i) {
-      Spot spot;
-      spot.name = config[i]["spot"]["name"].as<std::string>();
-      spot.pose.position.x = config[i]["spot"]["position"]["x"].as<float>();
-      spot.pose.position.y = config[i]["spot"]["position"]["y"].as<float>();
-      spot.pose.position.z = config[i]["spot"]["position"]["z"].as<float>();
-      spot.pose.orientation.x =
-        config[i]["spot"]["orientation"]["x"].as<float>();
-      spot.pose.orientation.y =
-        config[i]["spot"]["orientation"]["y"].as<float>();
-      spot.pose.orientation.z =
-        config[i]["spot"]["orientation"]["z"].as<float>();
-      spot.pose.orientation.w =
-        config[i]["spot"]["orientation"]["w"].as<float>();
-      spots_.push_back(spot);
-    }
-  }
-}
-
-/// @brief write spots into file
-void SpotManager::WriteIntoFile_() {
-  YAML::Node config;
-  for (size_t i = 0; i < spots_.size(); ++i) {
-    Spot& spot = spots_[i];
-    config[i]["spot"]["name"] = spot.name;
-    config[i]["spot"]["position"]["x"] = spot.pose.position.x;
-    config[i]["spot"]["position"]["y"] = spot.pose.position.y;
-    config[i]["spot"]["position"]["z"] = spot.pose.position.z;
-    config[i]["spot"]["orientation"]["x"] = spot.pose.orientation.x;
-    config[i]["spot"]["orientation"]["y"] = spot.pose.orientation.y;
-    config[i]["spot"]["orientation"]["z"] = spot.pose.orientation.z;
-    config[i]["spot"]["orientation"]["w"] = spot.pose.orientation.w;
-  }
-  std::ofstream ofs(file_, std::ios_base::trunc);
-  ofs << config;
-  ofs << std::endl;
-}
-
-/// @brief get index of spot
-/// @param _name spot name
-/// @return spot index, -1 if not found
-int SpotManager::GetIndex_(std::string _name) {
-  int result = -1;
-  int index = 0;
-
-  for (auto it: spots_) {
-    if (_name == it.name) {
-      result = index;
-      break;
-    }
-    ++index;
-  }
-  return result;
-}
-
-/// @brief get all spot names
-/// @return spot name list
-std::vector<std::string> SpotManager::GetList_() {
-  std::vector<std::string> res;
-  for (auto it: spots_) {
-    res.push_back(it.name);
-  }
-  return res;
-}
-
+/// @brief broadcast all spot as tf
 void SpotManager::BroadcastSpots(const ros::TimerEvent& ev)
 {
-  for (auto spot: spots_) {
+  for (auto spot: spot_list_.spots()) {
     tf::Transform transform;
     transform.setOrigin(tf::Vector3(spot.pose.position.x,
                                     spot.pose.position.y,
