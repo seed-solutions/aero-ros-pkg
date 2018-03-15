ab2d8b61718b686bb3e4640bd5c7abf5149e29df
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2018-03-15 20:06:40 +0900

    [aero_samples] remove deprecated methods

--- aero_samples/src/ik.cc
+++ aero_samples/src/ik.cc
@@ -8,14 +8,14 @@ int main(int argc, char **argv)
   // init ros
   ros::init(argc, argv, "ik_sample_node");
   ros::NodeHandle nh;
-  
+
   // init robot interface
   aero::interface::AeroMoveitInterfacePtr robot(new aero::interface::AeroMoveitInterface(nh));
   ROS_INFO("reseting robot pose");
   robot->sendResetManipPose();
   sleep(1);
 
-
+#if 0
   geometry_msgs::Pose pose1;// target pose
   pose1.position.x = 0.57;
   pose1.position.y = -0.15;
@@ -29,6 +29,20 @@ int main(int argc, char **argv)
   ROS_INFO("x:%f y%f z:%f", pose1.position.x, pose1.position.y, pose1.position.z);
   ROS_INFO("rot");
   ROS_INFO("w:%f x:%f y%f z:%f", pose1.orientation.w, pose1.orientation.x, pose1.orientation.y, pose1.orientation.z);
+#endif
+  //aero::Vector3 pos(0.57, -0.15, 1.1);
+  aero::Translation pos(0.57, -0.15, 1.1);
+  aero::Quaternion  rot(1.0, 0.0, 0.0, 0.0);
+  aero::Transform   pose1 = pos * rot;
+
+  ROS_INFO("ik target");
+  ROS_INFO("pos");
+  ROS_INFO("x:%f y:%f z:%f", pose1.translation().x(), pose1.translation().y(), pose1.translation().z());
+  ROS_INFO("rot");
+  {
+    aero::Quaternion qq(pose1.linear());
+    ROS_INFO("w:%f x:%f y:%f z:%f", qq.w(), qq.x(), qq.y(), qq.z());
+  }
 
   // setFromIK(arm, ikrange, pose, eef)
   // solve IK and set that answer to the robot model which interface has
