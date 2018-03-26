039ae57266ab29afcadc9f2cd3cc46bd9234bd1c
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2018-03-26 20:05:28 +0900

    [aero_std] update lookat_commander

--- aero_std/include/aero_std/AeroLookatCommander.hh
+++ aero_std/include/aero_std/AeroLookatCommander.hh
@@ -9,14 +9,22 @@ namespace aero
   {
     class AeroLookatCommander
     {
-    public: AeroLookatCommander(aero::interface::AeroMoveitInterface *_ami);
+    public: AeroLookatCommander(ros::NodeHandle &_nh,
+                                aero::interface::AeroMoveitInterface *_ami);
+
+    public: bool setTrackingMode(aero::tracking _mode, const aero::Vector3 &_pos);
+    public: bool setNeckRPY(double _r, double _p, double _y);
+    public: bool setLookAtTopic(const std::string &_topic);
 
     protected: ros::CallbackQueue eventqueue_;
     protected: void timerCallback(const ros::TimerEvent& ev);
 
-    protected: boost::mutex interface_mtx_;
+    protected: aero::tracking tracking_mode_;
+    protected: aero::Vector3 tracking_pos_;
+    protected: boost::mutex callback_mtx_;
     protected: robot_state::RobotStatePtr kinematic_state_;
     protected: aero::interface::AeroMoveitInterface::Ptr ami_;
+    protected: ros::Time previous_callback_;
     };
   }
 }
--- aero_std/src/AeroLookatCommander.cc
+++ aero_std/src/AeroLookatCommander.cc
@@ -1,6 +1,7 @@
 #include <aero_std/AeroLookatCommander.hh>
 
-aero::lookat_commander::AeroLookatCommander::AeroLookatCommander(aero::interface::AeroMoveitInterface *_ami)
+aero::lookat_commander::AeroLookatCommander::AeroLookatCommander(ros::NodeHandle &_nh,
+                                                                 aero::interface::AeroMoveitInterface *_ami)
 {
   ami_.reset(_ami);
   kinematic_state_ = robot_state::RobotStatePtr(new robot_state::RobotState(ami_->kinematic_model));
@@ -9,9 +10,75 @@ aero::lookat_commander::AeroLookatCommander::AeroLookatCommander(aero::interface
                     boost::bind(&aero::lookat_commander::AeroLookatCommander::timerCallback,
                                 this, _1),
                     &eventqueue_);
+  //
+}
+
+bool aero::lookat_commander::AeroLookatCommander::setTrackingMode(aero::tracking _mode, const aero::Vector3 &_pos)
+{
+  boost::mutex::scoped_lock lk(callback_mtx_);
+  tracking_mode_ = _mode;
+
+  switch(tracking_mode_) {
+  case aero::tracking::map:
+  case aero::tracking::map_static:
+    {
+      aero::Vector3 vec_in_base;
+      vec_in_base = ami_->volatileTransformToBase(_pos.x(), _pos.y(), _pos.z());
+      ami_->setRobotStateToCurrentState(kinematic_state_);
+      ami_->setLookAt_(vec_in_base.x(), vec_in_base.y(), vec_in_base.z(),
+                       kinematic_state_);
+      tracking_pos_ = _pos;
+    }
+    break;
+  case aero::tracking::base:
+  case aero::tracking::base_static:
+    ami_->setRobotStateToCurrentState(kinematic_state_);
+    ami_->setLookAt_(_pos.x(), _pos.y(), _pos.z(), kinematic_state_);
+    tracking_pos_ = _pos;
+    break;
+  default:
+    return false;
+    break;
+  }
+  ami_->sendNeckAsync_(2000, kinematic_state_);
+
+  return true;
+}
+
+bool aero::lookat_commander::AeroLookatCommander::setNeckRPY(double _r, double _p, double _y)
+{
+  ami_->setNeck_(_r, _p, _y, kinematic_state_);
+  ami_->sendNeckAsync_(2000, kinematic_state_);
+  return true;
+}
+
+bool aero::lookat_commander::AeroLookatCommander::setLookAtTopic(const std::string &_topic)
+{
+  // TODO:
+  return true;
 }
 
 void aero::lookat_commander::AeroLookatCommander::timerCallback(const ros::TimerEvent& ev)
 {
-
+  boost::mutex::scoped_lock lk(callback_mtx_);
+  //
+  switch(tracking_mode_) {
+  case aero::tracking::map:
+    {
+      aero::Vector3 vec_in_base;
+      vec_in_base = ami_->volatileTransformToBase(tracking_pos_.x(), tracking_pos_.y(), tracking_pos_.z());
+      ami_->setRobotStateToCurrentState(kinematic_state_);
+      ami_->setLookAt_(vec_in_base.x(), vec_in_base.y(), vec_in_base.z(),
+                       kinematic_state_);
+    }
+    break;
+  case aero::tracking::base:
+    ami_->setRobotStateToCurrentState(kinematic_state_);
+    ami_->setLookAt_(tracking_pos_.x(), tracking_pos_.y(), tracking_pos_.z(), kinematic_state_);
+    break;
+  default:
+    return;
+    break;
+  }
+  ami_->sendNeckAsync_(2000, kinematic_state_);
 }
