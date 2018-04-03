8ff8e563cff610676f58545bb41ca8bdfb2e6734
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2018-04-04 01:04:15 +0900

    [aero_description, scripts] fix

--- aero_description/scripts/create_eusmodel.sh
+++ aero_description/scripts/create_eusmodel.sh
@@ -10,7 +10,7 @@ aero_xacro=${aero_description_path}/robots/aero.urdf.xacro
 aero_urdf=${aero_description_path}/robots/aero.urdf
 ## aero_l=${aero_description_path}/robots/aero.l
 
-if [ -e ${aero_xacro} -a -e ${aero_yaml} ]
+if [ -e ${aero_xacro}  ]
 then
   rosrun xacro xacro.py ${aero_xacro} > ${aero_urdf}
 #  rosrun euscollada collada2eus_urdfmodel -I ${aero_urdf} -O ${aero_l} -C ${aero_yaml}
